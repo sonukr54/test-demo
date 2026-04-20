a2884bf980bb52a4d1d7556801f6885a48b159c8
sonukr54 <sonu93y93@gmail.com>
2026-04-20 11:29:16 +0530

    initial files added via intellij

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/Hello")
+    public String returnSomething(){
+
+        return "Hello from controller";
+    }
+}
