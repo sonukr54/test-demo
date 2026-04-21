f71f0e4d5dc0d5d108ce3821943400b74d989a37
sonukr54 <sonu93y93@gmail.com>
2026-04-21 16:24:28 +0530

    new features added

---
+++ src/main/java/com/example/demo/entity/Students.java
@@ -0,0 +1,58 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Students {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int rollNo;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public Students(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Students() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public int getRollNo() {
+        return rollNo;
+    }
+}
