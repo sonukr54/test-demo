26da43c0c2d36e9f43ef91b9c86491f10e573fc5
sonukr54 <sonu93y93@gmail.com>
2026-05-11 16:43:36 +0530

    field added

--- src/main/java/com/example/demo/entity/Students.java
+++ src/main/java/com/example/demo/entity/Students.java
@@ -15,6 +15,19 @@ public class Students {
     private String name;
     private int rollNo;
     private String address;
+    private String subject;
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Students(String subject) {
+        this.subject = subject;
+    }
 
     public String getName() {
         return name;
